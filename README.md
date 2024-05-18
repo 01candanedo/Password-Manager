c8bcdfb030d4d246257064226f930219beba32a9
01candanedo <01candanedo@gmail.com>
2024-05-18 08:29:07 +0000

    new version 0.2 login design

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# Password-Manager
-C++ Source Code to Create a Local Password Manager, Made in August, 2019 by @01candanedo. 
+# password-manager
+
+## About
+Originally created in August 2019 by @01candanedo, this C++ source code provided a local password management solution. Over time, technology evolves,>
+
+## New Features
+
+### Vue.js Integration: 
+We've rebuilt the password manager from the ground up using Vue.js, a progressive JavaScript framework, to provide a more dynamic and intuitive user >
+
+### Enhanced Security: 
+While maintaining the core security principles of the original version, our Vue.js-powered password manager offers additional layers of encryption an>
+
+### Improved User Interface: 
+Enjoy a modern and responsive interface designed to streamline your password management experience. With Vue.js, we've prioritized simplicity and usa>
+
+## Recommended IDE Setup
+
+[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
+
+## Type Support for `.vue` Imports in TS
+
+TypeScript cannot handle type information for `.vue` imports by default, so we replace the `tsc` CLI with `vue-tsc` for type checking. In editors, we need [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) to make the TypeScript language service aware of `.vue` types.
+
+## Customize configuration
+
+See [Vite Configuration Reference](https://vitejs.dev/config/).
+
+## Project Setup
+
+```sh
+bun install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+bun dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+bun build
+```
+
+### Run Headed Component Tests with [Cypress Component Testing](https://on.cypress.io/component)
+
+```sh
+bun test:unit:dev # or `bun test:unit` for headless testing
+```
+
+### Run End-to-End Tests with [Cypress](https://www.cypress.io/)
+
+```sh
+bun test:e2e:dev
+```
+
+This runs the end-to-end tests against the Vite development server.
+It is much faster than the production build.
+
+But it's still recommended to test the production build with `test:e2e` before deploying (e.g. in CI environments):
+
+```sh
+bun build
+bun test:e2e
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+bun lint
+```
---
+++ src/views/LoginView.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { useColorMode } from '@vueuse/core'
+import { Icon } from '@iconify/vue'
+import { Button } from '@/components/ui/button'
+import { useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import * as z from 'zod'
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { toast } from '@/components/ui/toast'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+
+const mode = useColorMode()
+
+const formSchema = toTypedSchema(
+  z.object({
+    username: z.string().min(2).max(50)
+  })
+)
+
+const { handleSubmit } = useForm({
+  validationSchema: formSchema
+})
+
+const onSubmit = handleSubmit((values) => {
+  toast({
+    title: 'You submitted the following values:',
+    description: h(
+      'pre',
+      { class: 'mt-2 w-[340px] rounded-md bg-slate-950 p-4' },
+      h('code', { class: 'text-white' }, JSON.stringify(values, null, 2))
+    )
+  })
+})
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline" class="mx-6 mt-6">
+        <Icon
+          icon="radix-icons:moon"
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <Icon
+          icon="radix-icons:sun"
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="mode = 'light'"> Light </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'dark'"> Dark </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'auto'"> System </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+
+  <div class="flex items-center justify-center min-h-screen">
+    <Card class="w-96">
+      <CardHeader>
+        <CardTitle class="mx-auto pb-4 text-2xl">Login</CardTitle>
+        <CardDescription class="text-base"
+          >Welcome to the new password manager system v0.2</CardDescription
+        >
+      </CardHeader>
+      <CardContent class="grid gap-4">
+        <form class="w-2/3 space-y-6 mx-auto" @submit="onSubmit">
+          <FormField v-slot="{ usernameField }" name="username">
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="Enter your username..." v-bind="usernameField" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <FormField v-slot="{ passwordField }" name="password">
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="Enter your password..." v-bind="passwordField" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+          <Button type="submit" class="w-full"> Sign In </Button>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <p class="text-sm">
+          Forgot your password? <a href="#" class="text-blue-600">Reset it here</a>
+        </p>
+      </CardFooter>
+    </Card>
+  </div>
+</template>
